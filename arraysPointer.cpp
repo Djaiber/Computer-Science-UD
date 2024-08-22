7723641af9b0b9d5bc2128e4740a2bad8ff73709
Jaiber Diaz <118369577+Djaiber@users.noreply.github.com>
2024-08-22 20:31:33 +0000

    primer ejercicio correccion

--- arraysPointer.cpp
+++ arraysPointer.cpp
@@ -4,19 +4,19 @@ int main()
 {
     int * notas; int n;
     printf("Ingrese cuantos datos: ");
-    scanf("%1", &n);
+    scanf("%i", &n);
     notas = new int[n];
     
 
-    for(int i=0; i<100;i++){
+    for(int i=0; i<n;i++){
     	
-      printf("Ingrese el dato %i", i);
+      printf("Ingrese el dato %i\n", i);
       scanf("%i", &notas[i]);
         
     }
     
-    for(int i=0; i<100;i++){
-		  printf("%i", &notas[i]);
+    for(int i=0; i<n;i++){
+		  printf("%i\n", &notas[i]);
         
     }
     
