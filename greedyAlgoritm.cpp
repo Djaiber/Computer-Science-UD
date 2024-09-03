0711a30c21ae5518df6414ba6a6671137faa55a1
Jaiber Diaz <118369577+Djaiber@users.noreply.github.com>
2024-09-03 21:48:31 +0000

    Quitar estructura vector

--- greedyAlgoritm.cpp
+++ greedyAlgoritm.cpp
@@ -1,7 +1,4 @@
-#include <vector>
 #include <cstdio>
-
-using namespace std;
 
 struct Evento {
     int id;
@@ -10,8 +7,7 @@ struct Evento {
 };
 
 // Función para ordenar los eventos por tiempo de finalización usando Bubble Sort
-void ordenarEventos(vector<Evento> &eventos) {
-    int n = eventos.size();
+void ordenarEventos(Evento eventos[], int n) {
     for (int i = 0; i < n-1; i++) {
         for (int j = 0; j < n-i-1; j++) {
             if (eventos[j].fin > eventos[j+1].fin) {
@@ -23,9 +19,9 @@ void ordenarEventos(vector<Evento> &eventos) {
     }
 }
 
-void seleccionarEventos(vector<Evento> &eventos) {
+void seleccionarEventos(Evento eventos[], int n) {
     // Ordenar los eventos por tiempo de finalización
-    ordenarEventos(eventos);
+    ordenarEventos(eventos, n);
 
     printf("Eventos seleccionados:\n");
     printf("ID\tInicio\tFin\n");
@@ -35,7 +31,7 @@ void seleccionarEventos(vector<Evento> &eventos) {
     printf("%i\t%i\t%i\n", eventos[i].id, eventos[i].inicio, eventos[i].fin);
 
     // Considerar los siguientes eventos
-    for (int j = 1; j < eventos.size(); j++) {
+    for (int j = 1; j < n; j++) {
         // Si este evento empieza después de que el último seleccionado terminó, seleccionarlo
         if (eventos[j].inicio >= eventos[i].fin) {
             printf("%i\t%i\t%i\n", eventos[j].id, eventos[j].inicio, eventos[j].fin);
@@ -45,26 +41,26 @@ void seleccionarEventos(vector<Evento> &eventos) {
 }
 
 int main() {
-    vector<Evento> eventos;
+    Evento eventos[100]; // Asumiendo que no habrá más de 100 eventos
+    int n = 0;
 
     // Abrir el archivo "eventos.txt" para lectura
-    FILE *file = fopen("eventos.txt", "r");
+    FILE *file = fopen("eventos1.txt", "r");
     if (file == NULL) {
         printf("No se pudo abrir el archivo eventos.txt\n");
         return 1;
     }
 
-    Evento evento;
-    // Leer cada línea del archivo y almacenar los datos en el vector de eventos
-    while (fscanf(file, "%i,%i,%i", &evento.id, &evento.inicio, &evento.fin) != EOF) {
-        eventos.push_back(evento);
+    // Leer cada línea del archivo y almacenar los datos en el array de eventos
+    while (fscanf(file, "%i,%i,%i", &eventos[n].id, &eventos[n].inicio, &eventos[n].fin) != EOF) {
+        n++;
     }
 
     // Cerrar el archivo
     fclose(file);
 
     // Seleccionar eventos que no se superpongan
-    seleccionarEventos(eventos);
+    seleccionarEventos(eventos, n);
 
     return 0;
 }
