e9080f46cc9a3731a65deed9c021f7db554d6deb
Jaiber Diaz <118369577+Djaiber@users.noreply.github.com>
2025-01-23 14:49:56 +0000

    puliendo codigo

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -5,9 +5,7 @@ using namespace std;
 struct Nodo {
 
     int dato; // Dato o valor
-
     struct Nodo *izq; // Apuntador al siguiente elemento de la lista
-
     struct Nodo *der; // Apuntador al elemento anterior de la lista
 
 };
@@ -15,27 +13,20 @@ struct Nodo {
 Nodo *arbol = NULL;
 
 Nodo *crearNodo(int);
-
 void insertarNodo(Nodo *&, int);
-
 void mostrarArbol(Nodo *, int);
 
 int main() {
 
     int dato, cont;
-
     cout << "Digite cuantos nodos desea ingresar: ";
-
     cin >> cont;
 
     for (int i = 0; i < cont; i++) {
 
         cout << "Digite el dato a guardar en un nodo: ";
-
         cin >> dato;
-
         insertarNodo(arbol, dato);
-
     }
 
     cout << "Mostrando arbol: " << endl;
@@ -49,13 +40,9 @@ int main() {
 Nodo *crearNodo(int n) {
 
     Nodo *nuevo_nodo = new Nodo();
-
     nuevo_nodo->dato = n;
-
     nuevo_nodo->izq = NULL;
-
     nuevo_nodo->der = NULL;
-
     return nuevo_nodo;
 
 }
@@ -63,9 +50,7 @@ Nodo *crearNodo(int n) {
 void insertarNodo(Nodo *&arbol, int n) {
 
     if (arbol == NULL) { // Si el arbol está vacío
-
         Nodo *nuevo_nodo = crearNodo(n);
-
         arbol = nuevo_nodo;
 
     } else {
@@ -73,39 +58,24 @@ void insertarNodo(Nodo *&arbol, int n) {
         int valorRaiz = arbol->dato; // Obtenemos el valor raíz
 
         if (n < valorRaiz) {
-
             insertarNodo(arbol->izq, n);
-
         } else {
-
             insertarNodo(arbol->der, n);
-
         }
-
     }
-
 }
 
 void mostrarArbol(Nodo *arbol, int cont) {
 
     if (arbol == NULL) {
-
         return; // Si es vacío, caso base
-
     } else {
-
         mostrarArbol(arbol->der, cont + 1);
-
         for (int i = 0; i < cont; i++) {
-
             cout << "   ";
-
         }
-
         cout << arbol->dato << endl;
-
         mostrarArbol(arbol->izq, cont + 1);
-
     }
 
 }
